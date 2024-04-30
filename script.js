0a438bbd8eb2fd0621f5654e1a258707158f25af
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-05-01 02:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+document.addEventListener('DOMContentLoaded', function() {
+    const list = document.getElementById('list');
+
+    function addMoreItems() {
+        for (let i = 0; i < 2; i++) {
+            const newItem = document.createElement('li');
+            newItem.textContent = 'List Item';
+            list.appendChild(newItem);
+        }
+    }
+
+    for (let i = 0; i < 10; i++) {
+        const newItem = document.createElement('li');
+        newItem.textContent = 'List Item';
+        list.appendChild(newItem);
+    }
+
+    window.addEventListener('scroll', function() {
+
+		if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            addMoreItems();
+        }
+    });
+});
 
